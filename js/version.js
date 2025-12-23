807a4d118b271bffe074172d1f5e7f4529324db1
Pixler <102816049+PixlerStudioOfficial@users.noreply.github.com>
2025-12-23 17:55:14 +0300

    Add files via upload

--- js/version.js
+++ js/version.js
@@ -1,4 +1,4 @@
-const PROJECT_VERSION = "v0.10.0c-alpha";
+const PROJECT_VERSION = "v0.10.1-alpha";
 
 function updateVersion() {
     const versionElement = document.getElementById('project-version');
